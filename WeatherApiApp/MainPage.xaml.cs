f74a5946bf92ce2d6d9a94627f16e269c5ca32bf
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-18 22:06:03 +0200

    before service changes

--- WeatherApiApp/MainPage.xaml.cs
+++ WeatherApiApp/MainPage.xaml.cs
@@ -9,16 +9,29 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+		/*		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+				if (count == 1)
+					CounterBtn.Text = $"Clicked {count} time";
+				else
+					CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+				SemanticScreenReader.Announce(CounterBtn.Text);
+		*/
+		await SendRequestAsync(new Uri("https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=Cj66O8OLTih8hPqA7AOKfevJuX11N1hp"));
+		Console.WriteLine("Hello, World!");
+
+
+	}
+	static async Task SendRequestAsync(Uri uri)
+	{
+		HttpResponseMessage response;
+		using (HttpClient client = new HttpClient())
+		{
+			response = await client.GetAsync(uri);
+		}
 	}
 }
 
