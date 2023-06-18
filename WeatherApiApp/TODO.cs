80505e4809e26d57f92099185e9b6a8a3091916b
invisloa <91217120+invisloa@users.noreply.github.com>
2023-06-19 00:26:54 +0200

    todo

--- WeatherApiApp/TODO.cs
+++ WeatherApiApp/TODO.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace WeatherApiApp
-{
-	internal class TODO
-	{
-	}
-}
+#region current
+// make at least one working weather view 
+//
+// ContentView => ContentPage Cast error
+#endregion
+
+
+#region highLater
+// Make viewmodels for every type of _dataService
+
+#endregion
+
+
+
+
+#region lowPriority
+// ApiWeather Location from my current location
+// check values in models (int-double-object)
+#endregion
+
+
+
+#region done
+// Make model for every method to get _dataService+
+#endregion
